d005dfed0aeb644f4a4790c881d583953116d38a
mk21b <iMelvinmarlowe@proton.me>
2025-11-28 14:24:25 +0200

    done

--- utils.c
+++ utils.c
@@ -1,5 +1,13 @@
 /*
- * This file is reserved for additional utility functions if needed
+ * utils.c - Utility functions for _printf
+ *
+ * This file is reserved for additional utility functions if needed.
  * Currently all functions are contained in _printf.c
  */
+
+void placeholder(void)
+{
+	/* Placeholder to avoid empty translation unit warning */
+}
+
 
